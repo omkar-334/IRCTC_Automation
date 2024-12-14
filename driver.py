e795c6df9b609a1156aa5ed00a0c69ef336d70e9
Omkar Kabde <omkarkabde@gmail.com>
2024-12-14 17:57:00 +0530

    selenium driver

---
+++ driver.py
@@ -0,0 +1,33 @@
+import os
+
+from selenium.webdriver import ChromeOptions
+from seleniumwire import webdriver
+
+
+def create_driver(user_agent=None, headless=True):
+    if not user_agent:
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/122.0.2365.92"
+        # user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/118.0"
+
+    options = ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
+    options.add_experimental_option("useAutomationExtension", False)
+    if headless:
+        options.add_argument("--headless")
+    options.add_argument("start-maximized")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-http2")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("log-level=3")
+    prefs = {
+        "profile.default_content_settings.popups": 0,
+        "download.default_directory": str(os.getcwd()),
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+    }
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=options)
+    driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": user_agent})
+    return driver
